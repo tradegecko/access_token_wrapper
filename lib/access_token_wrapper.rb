61160d486d819bf2ec3ee4371673175321d7279c
Bradley Priest <bradleypriest@gmail.com>
2013-04-19 17:55:24 +0800

    initial version

---
+++ lib/access_token_wrapper.rb
@@ -0,0 +1,24 @@
+require "access_token_wrapper/version"
+
+module AccessTokenWrapper
+  class Base
+    attr_reader :token
+
+    def initialize(token, &callback)
+      @token    = token
+      @callback = callback
+    end
+
+    def method_missing(method, *args, &block)
+      token.send(method, *args, &block)
+    rescue  OAuth2::Error => exception
+      @token = token.refresh!
+      @callback.call(token)
+      token.send(method, *args, &block)
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      token.respond_to?(method_name, include_private) || super
+    end
+  end
+end
