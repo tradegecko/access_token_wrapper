5f40b73ed5a1fd0ab465e4e359bb6f87be5e4725
Bradley Priest <bradleypriest@gmail.com>
2018-01-02 08:57:36 +1100

    Move Base class to it's own file

---
+++ lib/access_token_wrapper/base.rb
@@ -0,0 +1,27 @@
+module AccessTokenWrapper
+  class Base
+    NON_ERROR_CODES = [402, 404, 422, 414, 429, 500, 503]
+    attr_reader :raw_token
+
+    def initialize(raw_token, &callback)
+      @raw_token = raw_token
+      @callback  = callback
+    end
+
+    def method_missing(method_name, *args, &block)
+      @raw_token.send(method_name, *args, &block)
+    rescue OAuth2::Error => exception
+      if NON_ERROR_CODES.include?(exception.response.status)
+        raise exception
+      else
+        @raw_token = @raw_token.refresh!
+        @callback.call(@raw_token, exception)
+        @raw_token.send(method_name, *args, &block)
+      end
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      @raw_token.respond_to?(method_name, include_private) || super
+    end
+  end
+end
